5cc261a7c9c17b19b893df3b7664b79da2855b31
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-12-18 11:47:04 -0700

    fetched API for 12 random YuGiOh cards each refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0);
+  const [loading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  // const [cards, setCards] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  async function getYuGiOhCards() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=12&offset=0&sort=random"
+      );
+      const fetchedData = await response.json();
+      const simplifiedCards = fetchedData.data.map((card) => ({
+        name: card.name,
+        image: card.card_images[0].image_url_small,
+      }));
+
+      console.log(simplifiedCards);
+      setData(simplifiedCards);
+    } catch (error) {
+      setError(error ? Error.message : "Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getYuGiOhCards();
+  }, []);
+
+  // return (
+
+  // )
 }
 
-export default App
+export default App;
